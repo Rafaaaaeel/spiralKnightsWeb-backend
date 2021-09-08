345f7708cca3b39dd4379f29278999669d1814a3
Rafael Oliveira <rafaelopires@icloud.com>
2021-09-08 11:33:20 -0300

    Modifying the month input to not use hardcode

--- src/com/spiralKnights/Page.java
+++ src/com/spiralKnights/Page.java
@@ -2,6 +2,7 @@ package com.spiralKnights;
 
 public class Page {
 	private String termsOfService;
+	private String month;
 	
 	public String getTermsOfService() {
 		return termsOfService;
@@ -9,5 +10,13 @@ public class Page {
 	
 	public void setTermsOfService(String termsOfService) {
 		this.termsOfService = termsOfService;
+	}
+	
+	public String getMonth() {
+		return month;
+	}
+	
+	public void setMonth(String month) {
+		this.month = month;
 	}
 }
